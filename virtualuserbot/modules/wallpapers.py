55a2c947b7017e91d00014bb49ed83018a88be89
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-15 01:08:42 +0530

    Update wallpapers.py

--- virtualuserbot/modules/wallpapers.py
+++ virtualuserbot/modules/wallpapers.py
@@ -7,13 +7,15 @@ from virtualuserbot.googol_images import googleimagesdownload
 from virtualuserbot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
 
 
-@friday.on(friday_on_cmd(pattern="wpaper ?(.*)"))
-@friday.on(sudo_cmd(pattern="img ?(.*)", allow_sudo=True))
+@friday.on(friday_on_cmd(pattern="(wpaper|wallpaper|wp) ?(.*)"))
+@friday.on(sudo_cmd(pattern="(wpaper|wallpaper|wp) ?(.*)", allow_sudo=True))
 async def img_sampler(event):
+    if event.fwd_from:
+        return
     await edit_or_reply(event, "`Processing...`")
     reply = await event.get_reply_message()
-    if event.pattern_match.group(1):
-        queryo = event.pattern_match.group(1)
+    if event.pattern_match.group(2):
+        queryo = event.pattern_match.group(2)
     elif reply:
         queryo = reply.message
     else:
@@ -29,7 +31,7 @@ async def img_sampler(event):
         lim = lim.replace("lim=", "")
         query = query.replace("lim=" + lim[0], "")
     except IndexError:
-        lim = 5
+        lim = 10
     response = googleimagesdownload()
 
     # creating list of arguments
@@ -48,35 +50,6 @@ async def img_sampler(event):
     )
     shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
     await event.delete()
-
-    query = queryo + "ultra hd wallpaper"
-    lim = findall(r"lim=\d+", query)
-    # lim = event.pattern_match.group(1)
-    try:
-        lim = lim[0]
-        lim = lim.replace("lim=", "")
-        query = query.replace("lim=" + lim[0], "")
-    except IndexError:
-        lim = 3
-    response = googleimagesdownload()
-
-    # creating list of arguments
-    arguments = {
-        "keywords": query,
-        "limit": lim,
-        "format": "jpg",
-        "no_directory": "no_directory",
-    }
-
-    # passing the arguments to the function
-    paths = response.download(arguments)
-    lst = paths[0][query]
-    await event.client.send_file(
-        await event.client.get_input_entity(event.chat_id), lst
-    )
-    shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
-    await event.delete()
-
 
 CMD_HELP.update(
     {
