29847752765a61c895c78fec1eb4f37dd0a3fc93
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-14 16:55:10 +0530

    Update bot_alive.py

--- virtualuserbot/modules/assistant/bot_alive.py
+++ virtualuserbot/modules/assistant/bot_alive.py
@@ -16,19 +16,16 @@ from virtualuserbot import ALIVE_NAME
 from virtualuserbot.modules import currentversion
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/0e7a45ed44e17ce68d8cd.png"
+PM_IMG = "https://telegra.ph/file/d7556f9cfda00798a1a99.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
-pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
-pm_caption += "➥ **Python:** `3.7.4` \n"
 pm_caption += "➥ **Database Status:**  `Functional`\n"
 pm_caption += "➥ **Current Branch** : `master`\n"
 pm_caption += f"➥ **Version** : `{currentversion}`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
-pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [InukaASiTH](GitHub.com/Inukaasith)\n"
-pm_caption += "[Assistant By Inukaasith](https://t.me/Inukaasith)"
+pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/ImJanindu/VirtualUserbot/blob/master/LICENSE)\n"
+pm_caption += "➥ **Copyright** : By [ImJanindu](GitHub.com/ImJanindu)\n"
+pm_caption += "[Assistant By Janindu 🇱🇰](https://t.me/imjanindu)"
 
 # only Owner Can Use it
 @assistant_cmd("alive", is_args=False)
